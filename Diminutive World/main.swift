dfcb5fb127a5548c081b1dffbd4b314aa245e31e
amwoods3 <amwoods3@cougars.ccis.edu>
2017-03-03 00:09:04 +0900

    Initial Commit

---
+++ Diminutive World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Diminutive World
+//
+//  Created by Andrew Woods on 2017/03/03.
+//  Copyright © 2017年 Andrew Woods. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
